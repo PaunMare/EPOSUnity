f3abaea4fcf74fdb947d2e101b9537f370da1842
Aleksa Trampa <alektramp@gmail.com>
2020-08-18 17:12:32 +0200

    Email factory for sending html body main

---
+++ Assets/EmailFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+public class EmailFactory : MonoBehaviour
+{
+    public InputField recipientEmail;
+
+    public void SendEmail()
+    {
+        MailMessage mail = new MailMessage();
+        SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+        SmtpServer.Timeout = 10000;
+        SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+        SmtpServer.UseDefaultCredentials = false;
+        SmtpServer.Port = 587;
+
+        mail.From = new MailAddress("eposprojekatunity@gmail.com");
+        mail.To.Add(new MailAddress(recipientEmail.text));
+        
+        mail.Subject = "Test Email through C Sharp App";
+        mail.Body = "<h2>test mail</h2>";
+        mail.IsBodyHtml = true;
+        
+
+        SmtpServer.Credentials = new System.Net.NetworkCredential("eposprojekatunity@gmail.com", "pauntrampa") as ICredentialsByHost; SmtpServer.EnableSsl = true;
+        ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        };
+
+        mail.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+        SmtpServer.Send(mail);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SendEmail();
+        }
+    }
+}
